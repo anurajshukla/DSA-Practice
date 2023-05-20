04389672dc4d0fb3bdb978abbf83eb922287ee42
Anuraj Shukla <89661673+anurajshukla@users.noreply.github.com>
2023-05-20 23:11:31 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (39.10%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        if(n==1) { return 2;}
+        for(int i=n; i<=n*n; i++) {
+            if(i%2==0 && i%n==0) { return i;}
+        }
+        return -1;
+    }
+}

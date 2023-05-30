b9ac4808484426f6819c64f20399646847cf392c
Anuraj Shukla <89661673+anurajshukla@users.noreply.github.com>
2023-05-30 18:54:51 +0530

    Time: 1 ms (100.00%), Space: 40.3 MB (14.95%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if(dividend == Integer.MIN_VALUE && divisor == -1){
+            return Integer.MAX_VALUE;
+        }
+        boolean neg = (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0);
+        dividend = Math.abs(dividend);
+        divisor = Math.abs(divisor);
+        int r = 0;
+        while(dividend - divisor >= 0){
+            int count = 0;
+
+            while(dividend - (divisor << 1 << count) >= 0){
+                count++;
+            }
+            r += 1 << count;
+            dividend -= divisor << count;
+        }
+
+        if(neg){
+            r = ~r + 1;
+        }
+        return r;
+    }
+}

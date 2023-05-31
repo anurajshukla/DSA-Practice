8cbbf8dfe595bde5a4bf82ceb7633095dee848c2
Anuraj Shukla <89661673+anurajshukla@users.noreply.github.com>
2023-06-01 00:29:55 +0530

    Time: 286 ms (11.24%), Space: 48.3 MB (15.65%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,15 +1,10 @@
 class Solution {
     public int countPrimes(int n) {
-        
-        if(n<2) { 
-            return 0; 
-        }
-        
+        if(n<2) { return 0; }
         boolean a[] = new boolean[n+1];
         Arrays.fill(a, true);
         a[0] = false; 
         a[1] = false;
-        
         for(int i=2; i*i <= n; i++) {
             for(int j = 2*i; j<=n; j+=i) {
                 a[j] = false;
@@ -17,9 +12,7 @@ class Solution {
         }
         int count =0;
         for(int i=0; i<n; i++) {
-            if(a[i] == true) { 
-                count++;
-            }
+            if(a[i] == true) { count++; }
         }
         return count;
     }

9b3a9775d1f57fcd7c56cc70ec3d0188ee13fbb7
Anuraj Shukla <89661673+anurajshukla@users.noreply.github.com>
2023-06-06 03:00:24 +0530

    Time: 0 ms (100.00%), Space: 43 MB (24.50%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -4,20 +4,16 @@ class Solution {
         while(s<=e) {
             int mid = s + (e-s)/2;
             if(nums[mid]==target) {return true;}
-            
             if(nums[s] < nums[mid]) {
                 if(nums[s] <= target && target < nums[mid]) { e=mid-1; }
                 else { s=mid+1; }
             }
-            
             else if(nums[s]>nums[mid]) {
                 if( nums[mid] < target && target<=nums[e] ) { s=mid+1; }
                 else { e=mid-1; }
             }
-            
             else { s++; }
         }
-        
         return false;
     }
 }

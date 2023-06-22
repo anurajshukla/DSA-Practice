cd10e9bb8339825fc62b83bda9fa45baf1931515
Anuraj Shukla <89661673+anurajshukla@users.noreply.github.com>
2023-06-23 04:02:47 +0530

    Time: 1 ms (76.42%), Space: 40.9 MB (34.94%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<String> chars = new Stack<>();
+        Stack<Integer> nums = new Stack<>();
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == ']') { 
+                int n = nums.pop(); 
+                String ch = chars.pop();
+                String a = "";
+                while(!ch.equals("[")) {
+                    a = ch + a;
+                    ch = chars.pop();
+                }
+                String b = "";
+                while(n > 0) {
+                    b = b + a;
+                    n--;
+                }
+                chars.push(b);
+            }
+            else if(Character.isDigit(s.charAt(i))){
+                int st = i, e = s.indexOf('[', i);
+                nums.push(Integer.parseInt(s.substring(st, e)));
+                i = e - 1;
+            }
+            else{
+                chars.push(String.valueOf(s.charAt(i)));
+            }
+        }
+        String result="";
+         while(!chars.empty()){
+            result = chars.pop() + result;
+         }
+        return result;
+
+    }
+}

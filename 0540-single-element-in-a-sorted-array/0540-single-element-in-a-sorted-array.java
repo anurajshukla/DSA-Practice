59213e12545f8a6b9f214bbdebd7fcff345c46cc
Anuraj Shukla <89661673+anurajshukla@users.noreply.github.com>
2023-07-02 00:07:12 +0530

    Time: 0 ms (100.00%), Space: 50.4 MB (15.78%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,22 +1,26 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        
+        //boundary cases
         if(nums.length == 1) { return nums[0]; }
-        if(nums[0]!=nums[1]) { return nums[0]; }
-        if(nums[nums.length-1] != nums[nums.length-2]) return nums[nums.length-1];
-
-        int s=0, e = nums.length-1, mid=0;
-        while(s<= e) {
+        if(nums[0] != nums[1]) { return nums[0]; }
+        if(nums[n-2] != nums[n-1]) { return nums[n-1]; }
+        
+        int s=1, e=n-1, mid=0;
+        while(s <= e) {
             mid = s + (e-s)/2;
-            if(nums[mid] != nums[mid -1] && nums[mid] !=  nums[mid +1]) { return nums[mid]; }
-            if(mid % 2 == 0){ 
-                if(nums[mid] == nums[mid + 1]) { s = mid +2; }
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) { 
+                return nums[mid]; 
+            }
+            if(mid%2 == 0) {
+                if(nums[mid] == nums[mid+1]) { s = mid +2; }
                 else { e = mid; }
             }
             else {
-                if(nums[mid] == nums[mid -1]) { s = mid + 1; }
-                else { e = mid; } 
+                if(nums[mid] == nums[mid-1]) { s = mid +1; }
+                else { e = mid; }
             }
-            
         }
         return -1;
     }

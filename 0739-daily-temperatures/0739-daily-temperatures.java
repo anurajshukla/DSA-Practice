265117aa4c970996d304d1c630e7053175e14313
Anuraj Shukla <89661673+anurajshukla@users.noreply.github.com>
2023-07-06 18:54:13 +0530

    Time: 191 ms (18.24%), Space: 61.9 MB (5.15%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -3,10 +3,16 @@ class Solution {
         int n = temperatures.length;
         Stack<Integer> stack = new Stack<>();
         int[] ans = new int[n];
-        for(int i=0; i<n; i++) {
-            while(!stack.isEmpty() && (temperatures[i] > temperatures[stack.peek()])) {
-                ans[stack.peek()] = i- stack.pop();
+        for(int i=n-1; i>=0; i--) {
+            // to find greater element in than temperatures[i] in the stack
+            while(!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]) {
+                stack.pop();
             }
+            // found the greater element and adding diff to ans array
+            if(!stack.isEmpty()) { 
+                ans[i]= stack.peek() - i; 
+            }
+            // pushing the index of temperatures[i] to stack
             stack.push(i);
         }
         return ans;
